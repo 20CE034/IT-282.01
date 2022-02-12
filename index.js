9e296350ac4e8eebf1b2a59e2348a1060de2f077
Dev Gundalia <94553525+20CE034@users.noreply.github.com>
2022-02-12 10:30:24 +0530

    update (#1)

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+// Dark mode
+var icon=document.getElementById("moontosun");
+icon.onclick=function(){
+  console.log("Dark mode on");
+  document.body.classList.toggle("dark-mode");
+}
